72773f5bed73f2df2d0bdae0b48d0374ea47b116
James Saunders-Wyndham <kyotojim2@gmail.com>
2024-04-20 23:23:13 +0900

    Fix styling issues on 15 inch screen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,26 @@
 const Navbar = () => {
-	return (
-		<nav className="hidden md:flex w-full h-16 bg-black justify-center py-14 md:px-6">
-			<ul className="w-fit text-white text-sm sm:text-md md:text-lg lg:text-2xl font-robotoSlab flex flex-row justify-around md:justify-between items-center">
-				<li className="w-25 md:w-50 li-hover relative border-white border-r-2 inline-block transition-all duration-500 hover:text-red-500 hover:cursor-pointer">
-					<span className="text-content">ING BAR</span>
-					<span className="text-hover-content">イング居酒屋</span>
-				</li>
-				<li className="li-hover relative border-white border-r-2 inline-block transition-all w-25 md:w-50 duration-500 hover:text-red-500 hover:cursor-pointer">
-					<span className="text-content ">MENU</span>
-					<span className="text-hover-content ">メニュー</span>
-				</li>
-				<li className="li-hover relative border-white border-r-2 inline-block transition-all w-25 md:w-50 duration-500 hover:text-red-500 hover:cursor-pointer">
-					<span className="text-content ">CONTACT</span>
-					<span className="text-hover-content ">連絡</span>
-				</li>
-				<li className="li-hover relative inline-block transition-all duration-500 w-25 md:w-50 hover:text-red-500 hover:cursor-pointer">
-					<span className="text-content ">ACCESS</span>
-					<span className="text-hover-content ">アクセス</span>
-				</li>
-			</ul>
-		</nav>
-	);
+  return (
+    <nav className="hidden md:flex w-full h-16 bg-black justify-center py-14 lg:py-10 md:px-6">
+      <ul className="w-fit text-white text-sm sm:text-md md:text-lg lg:text-2xl font-robotoSlab flex flex-row justify-around md:justify-between items-center">
+        <li className="w-25 md:w-50 li-hover relative border-white border-r-2 inline-block transition-all duration-500 hover:text-red-500 hover:cursor-pointer">
+          <span className="text-content">ING BAR</span>
+          <span className="text-hover-content">イング居酒屋</span>
+        </li>
+        <li className="li-hover relative border-white border-r-2 inline-block transition-all w-25 md:w-50 duration-500 hover:text-red-500 hover:cursor-pointer">
+          <span className="text-content ">MENU</span>
+          <span className="text-hover-content ">メニュー</span>
+        </li>
+        <li className="li-hover relative border-white border-r-2 inline-block transition-all w-25 md:w-50 duration-500 hover:text-red-500 hover:cursor-pointer">
+          <span className="text-content ">CONTACT</span>
+          <span className="text-hover-content ">連絡</span>
+        </li>
+        <li className="li-hover relative inline-block transition-all duration-500 w-25 md:w-50 hover:text-red-500 hover:cursor-pointer">
+          <span className="text-content ">ACCESS</span>
+          <span className="text-hover-content ">アクセス</span>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
