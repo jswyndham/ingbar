e6bef3330cb442052bdea87c1f07f773f06d3a14
James Saunders-Wyndham <kyotojim2@gmail.com>
2024-05-08 18:07:18 +0900

    Add close menu click event outside the div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,119 +1,124 @@
-import React, { useEffect, useState } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
-import logo from '../images/white-logo-main.png';
-import logoBackImage from '../images/pexels-edwardeyer-ing-home-sm.jpg';
-import ImageCarousel from '../components/ImageCarousel';
-import OpeningHours from '../components/OpeningHours';
-import AboutUs from '../components/AboutUs';
-import Footer from '../components/Footer';
-import ReviewsCarousel from '../components/ReviewsCarousel';
+import { useEffect, useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import logo from "../images/white-logo-main.png";
+import logoBackImage from "../images/pexels-edwardeyer-ing-home-sm.jpg";
+import ImageCarousel from "../components/ImageCarousel";
+import OpeningHours from "../components/OpeningHours";
+import AboutUs from "../components/AboutUs";
+import Footer from "../components/Footer";
+import ReviewsCarousel from "../components/ReviewsCarousel";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
-	const [animationPhase, setAnimationPhase] = useState('showLogo');
+  const [animationPhase, setAnimationPhase] = useState("showLogo");
 
-	useEffect(() => {
-		const timeoutIds = [
-			setTimeout(() => {
-				setAnimationPhase('transition'); // Start fading logo out and fading background in
-			}, 2000),
-			setTimeout(() => {
-				setAnimationPhase('showContent'); // Fully display the content
-			}, 3000),
-		];
-		return () => timeoutIds.forEach(clearTimeout);
-	}, []);
+  useEffect(() => {
+    const timeoutIds = [
+      setTimeout(() => {
+        setAnimationPhase("transition"); // Start fading logo out and fading background in
+      }, 2000),
+      setTimeout(() => {
+        setAnimationPhase("showContent"); // Fully display the content
+      }, 3000),
+    ];
+    return () => timeoutIds.forEach(clearTimeout);
+  }, []);
 
-	const logoVariants = {
-		hidden: { opacity: 0 }, // Ensure this state is defined if used
-		show: { opacity: 1, y: 0, scale: 1.5, transition: { duration: 1 } },
-		hide: { opacity: 0, y: -200, scale: 1, transition: { duration: 1.75 } },
-	};
+  const logoVariants = {
+    hidden: { opacity: 0 }, // Ensure this state is defined if used
+    show: { opacity: 1, y: 0, scale: 1.5, transition: { duration: 1 } },
+    hide: { opacity: 0, y: -200, scale: 1, transition: { duration: 1.75 } },
+  };
 
-	const backgroundVariants = {
-		hidden: { opacity: 0 },
-		visible: { opacity: 1, transition: { duration: 2 } },
-	};
+  const backgroundVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 2 } },
+  };
 
-	const contentFadeInVariants = {
-		hidden: { opacity: 0 },
-		visible: { opacity: 1, transition: { duration: 2 } },
-	};
+  const contentFadeInVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 2 } },
+  };
 
-	return (
-		<motion.article
-			className="relative overflow-hidden w-screen h-full bg-black my-12 md:m-auto"
-			onViewportEnter={() => console.log('Entered viewport!')}
-			onViewportLeave={() => console.log('Left viewport!')}
-			whileInView={{ opacity: 1 }}
-			viewport={{ once: true }}
-		>
-			<AnimatePresence>
-				{animationPhase !== 'showContent' && (
-					<motion.img
-						src={logo}
-						alt="Main Logo"
-						initial="show"
-						whileInView="visible"
-						viewport={{ once: true }}
-						animate={
-							animationPhase === 'showLogo' ? 'show' : 'hide'
-						}
-						variants={logoVariants}
-						className="flex inset-0 m-auto py-40 md:py-60 w-4xl md:w-7/12 xl:w-7/12 3xl:w-5/12"
-					/>
-				)}
-			</AnimatePresence>
+  return (
+    <motion.article
+      className="relative overflow-hidden w-screen h-full bg-black my-12 md:m-auto"
+      onViewportEnter={() => console.log("Entered viewport!")}
+      onViewportLeave={() => console.log("Left viewport!")}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+    >
+      <Helmet>
+        <title>Rock Bar ING Kyoto - Home</title>
+        <meta
+          name="description"
+          content="Welcome to Rock Bar ING, the best late-night bar in Kyoto, Japan, where rock music is life and the drinks keep flowing. Located on Kiyamachi Street, Kyoto city."
+        />
+      </Helmet>
 
-			{animationPhase === 'showContent' && (
-				<>
-					<motion.img
-						src={logoBackImage}
-						alt="Background"
-						initial="hidden"
-						whileInView="visible"
-						viewport={{ once: true }}
-						animate="visible"
-						variants={backgroundVariants}
-						className="w-full h-auto"
-					/>
-					<motion.img
-						src={logo}
-						alt="Main Logo"
-						initial="hidden"
-						animate={
-							animationPhase === 'showContent' ? 'show' : 'hide'
-						}
-						variants={logoVariants}
-						className="absolute inset-0 mx-auto w-8/12 md:max-w-3xl pt-6 md:pt-20"
-					/>
-					<motion.div
-						initial="hidden"
-						whileInView="visible"
-						viewport={{ once: true }}
-						animate="visible"
-						variants={contentFadeInVariants}
-						className="relative mt-6 lg:mt-[-40px] xl:mt-[-60px] 2xl:mt-[-275px] 3xl:mt-[-550px] 4xl:mt-[-650px] 5xl:mt-[-875px]"
-					>
-						<div className="mb-20 lg:mb-24">
-							<ImageCarousel />
-						</div>
-						<div className="flex flex-col md:flex-row-reverse justify-center xl:justify-between items-center max-w-7xl mx-auto">
-							<div className="xl:w-2/5 md:mx-2 xl:mr-12 mx-1 my-2 flex justify-center border border-slate-700">
-								<OpeningHours />
-							</div>
-							<div className="w-full xl:w-3/5 md:mx-2 py-2 xl:pr-24 mx-1">
-								<AboutUs />
-							</div>
-						</div>
-						<div className="mx-2 lg:mt-24 mb-32">
-							<ReviewsCarousel />
-						</div>
-						<Footer />
-					</motion.div>
-				</>
-			)}
-		</motion.article>
-	);
+      <AnimatePresence>
+        {animationPhase !== "showContent" && (
+          <motion.img
+            src={logo}
+            alt="Main Logo"
+            initial="show"
+            whileInView="visible"
+            viewport={{ once: true }}
+            animate={animationPhase === "showLogo" ? "show" : "hide"}
+            variants={logoVariants}
+            className="flex inset-0 m-auto py-40 md:py-60 w-4xl md:w-7/12 xl:w-7/12 3xl:w-5/12"
+          />
+        )}
+      </AnimatePresence>
+
+      {animationPhase === "showContent" && (
+        <>
+          <motion.img
+            src={logoBackImage}
+            alt="Background"
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            animate="visible"
+            variants={backgroundVariants}
+            className="w-full h-auto"
+          />
+          <motion.img
+            src={logo}
+            alt="Main Logo"
+            initial="hidden"
+            animate={animationPhase === "showContent" ? "show" : "hide"}
+            variants={logoVariants}
+            className="absolute inset-0 mx-auto w-8/12 md:max-w-3xl pt-6 md:pt-20"
+          />
+          <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            animate="visible"
+            variants={contentFadeInVariants}
+            className="relative mt-6 lg:mt-[-40px] xl:mt-[-60px] 2xl:mt-[-275px] 3xl:mt-[-550px] 4xl:mt-[-650px] 5xl:mt-[-875px]"
+          >
+            <div className="mb-20 lg:mb-24">
+              <ImageCarousel />
+            </div>
+            <div className="flex flex-col md:flex-row-reverse justify-center xl:justify-between items-center max-w-7xl mx-auto">
+              <div className="xl:w-2/5 md:mx-2 xl:mr-12 mx-1 my-2 flex justify-center border border-slate-700">
+                <OpeningHours />
+              </div>
+              <div className="w-full xl:w-3/5 md:mx-2 py-2 xl:pr-24 mx-1">
+                <AboutUs />
+              </div>
+            </div>
+            <div className="mx-2 lg:mt-24 mb-32">
+              <ReviewsCarousel />
+            </div>
+            <Footer />
+          </motion.div>
+        </>
+      )}
+    </motion.article>
+  );
 };
 
 export default Home;
--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,57 +1,60 @@
-import logo from '../images/ing-large-white-transparent.png';
-import backgroundImg from '../images/ing-records.jpg';
-import BarSnacksList from '../components/menu/BarSnacksList';
-import BeerList from '../components/menu/BeerList';
-import SpiritsList from '../components/menu/SpiritsList';
-import WineList from '../components/menu/WineList';
-import BarMealsList from '../components/menu/BarMeals';
-import JapaneseAlcoholList from '../components/menu/JapaneseAlcoholList';
-import NonAlcoholicList from '../components/menu/NonAlcoholicList';
-import WhiskeyList from '../components/menu/WhiskeyList';
+import logo from "../images/ing-large-white-transparent.png";
+import backgroundImg from "../images/ing-records.jpg";
+import BarSnacksList from "../components/menu/BarSnacksList";
+import BeerList from "../components/menu/BeerList";
+import SpiritsList from "../components/menu/SpiritsList";
+import WineList from "../components/menu/WineList";
+import BarMealsList from "../components/menu/BarMeals";
+import JapaneseAlcoholList from "../components/menu/JapaneseAlcoholList";
+import NonAlcoholicList from "../components/menu/NonAlcoholicList";
+import WhiskeyList from "../components/menu/WhiskeyList";
+import { Helmet } from "react-helmet";
 
 function Menu() {
-	return (
-		<article
-			style={{
-				backgroundImage: `url(${backgroundImg})`,
-				backgroundSize: 'cover',
-				backgroundPosition: 'center',
-				backgroundAttachment: 'fixed',
-			}}
-			className="relative w-screen h-screen overflow-hidden"
-		>
-			<div className="absolute -top-8 lg:top-0 left-0 w-full h-full overflow-auto bg-black bg-opacity-60 z-40 flex flex-col">
-				<div className="flex justify-center">
-					<div className="h-fit w-10/12 md:w-9/12 lg:w-7/12 2xl:w-5/12 p-12 mt-28 mb-16 bg-gray-400 bg-opacity-40 backdrop-blur-sm flex flex-row justify-center rounded-lg shadow-xl shadow-slate-900">
-						<img
-							src={logo}
-							alt="ing logo"
-							className="h-14 md:h-20"
-						/>
-						<h2 className="font-serif text-5xl md:text-7xl px-4 mt-1">
-							Menu
-						</h2>
-					</div>
-				</div>
+  return (
+    <article
+      style={{
+        backgroundImage: `url(${backgroundImg})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundAttachment: "fixed",
+      }}
+      className="relative w-screen h-screen overflow-hidden"
+    >
+      <div className="absolute -top-8 lg:top-0 left-0 w-full h-full overflow-auto bg-black bg-opacity-60 z-40 flex flex-col">
+        <Helmet>
+          <title>Rock Bar ING Kyoto - Menu</title>
+          <meta
+            name="description"
+            content="Welcome to Rock Bar ING, the best late-night bar in Kyoto, Japan, where rock music is life and the drinks keep flowing. Located on Kiyamachi Street, Kyoto city."
+          />
+        </Helmet>
 
-				<BarMealsList />
+        <div className="flex justify-center">
+          <div className="h-fit w-10/12 md:w-9/12 lg:w-7/12 2xl:w-5/12 p-12 mt-28 mb-16 bg-gray-400 bg-opacity-40 backdrop-blur-sm flex flex-row justify-center rounded-lg shadow-xl shadow-slate-900">
+            <img src={logo} alt="ing logo" className="h-14 md:h-20" />
+            <h2 className="font-serif text-5xl md:text-7xl px-4 mt-1">Menu</h2>
+          </div>
+        </div>
 
-				<BarSnacksList />
+        <BarMealsList />
 
-				<BeerList />
+        <BarSnacksList />
 
-				<WineList />
+        <BeerList />
 
-				<JapaneseAlcoholList />
+        <WineList />
 
-				<WhiskeyList />
+        <JapaneseAlcoholList />
 
-				{/* <SpiritsList /> */}
+        <WhiskeyList />
 
-				<NonAlcoholicList />
-			</div>
-		</article>
-	);
+        {/* <SpiritsList /> */}
+
+        <NonAlcoholicList />
+      </div>
+    </article>
+  );
 }
 
 export default Menu;
