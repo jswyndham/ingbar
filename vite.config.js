77f7dee05d1602692386bb1e0b96139595ac846e
James Saunders-Wyndham <kyotojim2@gmail.com>
2024-05-16 11:34:01 +0900

    Fix deploy issue with base /ingbar/

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import postcss from "postcss";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/ingbar/',
+  base: "/ingbar/",
   css: {
     postcss,
   },
