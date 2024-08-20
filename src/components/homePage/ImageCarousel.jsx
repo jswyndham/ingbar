b229afc32190dffdfc727c124c02dfb1d895fd72
jswyndham <kyotojim2@gmail.com>
2024-08-20 12:44:22 +0900

    Update loading lazy for images

--- src/components/homePage/ImageCarousel.jsx
+++ src/components/homePage/ImageCarousel.jsx
@@ -1,49 +1,50 @@
-import { useEffect, useState } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import imageCarouselSchema from "../../schema/imageCarousel/imageCarouselSchema";
+import { useEffect, useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import imageCarouselSchema from '../../schema/imageCarousel/imageCarouselSchema';
 
 const ImageCarousel = () => {
-  const [key, setKey] = useState(0);
+	const [key, setKey] = useState(0);
 
-  const images = imageCarouselSchema();
+	const images = imageCarouselSchema();
 
-  useEffect(() => {
-    const handleResize = () => {
-      // Reinitialize the component key, causing a re-render to better handle re-sizing adjustment
-      setKey((prevKey) => prevKey + 1);
-    };
+	useEffect(() => {
+		const handleResize = () => {
+			// Reinitialize the component key, causing a re-render to better handle re-sizing adjustment
+			setKey((prevKey) => prevKey + 1);
+		};
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+		window.addEventListener('resize', handleResize);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 1000,
-    fade: true, // photo fade selection
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-  };
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 1000,
+		fade: true, // photo fade selection
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 4000,
+	};
 
-  return (
-    <Slider {...settings}>
-      {images.map((image, index) => (
-        <div key={index}>
-          <img
-            title={image.title}
-            src={image.image}
-            alt={`Slide - ${image.alt}`}
-            className="max-w-full xl:max-w-7xl mx-auto"
-          />
-        </div>
-      ))}
-    </Slider>
-  );
+	return (
+		<Slider {...settings}>
+			{images.map((image, index) => (
+				<div key={index}>
+					<img
+						title={image.title}
+						src={image.image}
+						alt={`Slide - ${image.alt}`}
+						className="max-w-full xl:max-w-7xl mx-auto"
+						loading="lazy"
+					/>
+				</div>
+			))}
+		</Slider>
+	);
 };
 
 export default ImageCarousel;
