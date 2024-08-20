1b2ad91975c30ad9a3d3ea2e1322c30888eb27da
jswyndham <kyotojim2@gmail.com>
2024-08-20 11:38:10 +0900

    Include legacy code package

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import postcss from "postcss";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
+import postcss from 'postcss';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss,
-  },
-  build: {
-    outDir: "dist", // This is the default value
-  },
+	plugins: [
+		react(),
+		legacy({
+			targets: ['defaults', 'not IE 11'], // Modify to include older browsers
+			additionalLegacyPolyfills: ['regenerator-runtime/runtime'],
+		}),
+	],
+	css: {
+		postcss,
+	},
+	build: {
+		outDir: 'dist',
+	},
 });
